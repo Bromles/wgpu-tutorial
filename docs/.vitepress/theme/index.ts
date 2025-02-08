ec5f51ad506bb5af9d0ccf97a6b20d4a19875f69
Aleksandr Ushakov <all.bromles@gmail.com>
2025-02-08 03:40:57 +0300

    update theme

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,5 +1,8 @@
 import DefaultTheme from 'vitepress/theme'
+import type {Theme} from 'vitepress'
 import './style.css'
 
 // noinspection JSUnusedGlobalSymbols
-export default DefaultTheme
+export default {
+    extends: DefaultTheme
+} satisfies Theme
